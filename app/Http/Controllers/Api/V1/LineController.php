c964e8599b29034e6ef2e98913636ea749bc2be2
kth616 <nitraid.0106@gmail.com>
2022-03-24 15:15:03 +0900

    天気予報の実装

--- app/Http/Controllers/Api/V1/LineController.php
+++ app/Http/Controllers/Api/V1/LineController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\ExternalApis\WeatherForecastApi\OpenWeather;
 use App\Http\Controllers\Controller;
 use App\Services\Deliverer;
 use App\Services\ReplyMessageGenerator;
 use App\Services\RequestParser;
+use App\Services\WeatherForecaster;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
@@ -24,6 +26,7 @@ class LineController extends Controller
         return response()->json(['message' => 'sent']);
     }
 
+
     // メッセージを受け取って返信
     public function callback(Request $request)
     {
@@ -36,11 +39,11 @@ class LineController extends Controller
             return response()->json(['message' => 'recieved(no events)']);
         }
         
-        $generator = new ReplyMessageGenerator;
+        $replyMessageGenerator = $this->makeReplyMessageGenerator();
         $deliverer = new Deliverer(env('LINE_CHANNEL_ACCESS_TOKEN'), env('LINE_CHANNEL_SECRET'));
         foreach ($recievedMessages as $recievedMessage){
             // 2. 受け取ったメッセージの内容から返信するメッセージを生成
-            $replyMessage = $generator->generate($recievedMessage->getText());
+            $replyMessage = $replyMessageGenerator->generate($recievedMessage->getText());
 
             // 3. 返信メッセージを返信先に送信
             $deliverer->reply($recievedMessage->getReplyToken(), $replyMessage);
@@ -48,5 +51,21 @@ class LineController extends Controller
         }
 
         return response()->json(['message' => 'received']);
+    }
+
+    public function makeReplyMessageGenerator()
+    {
+        // ここを入れ替えると別の天気予報APIを使うこともできる
+        $openWeatherMap = new OpenWeather(env('OPENWEATHERMAP_API_KEY'));
+
+        // FIXME: メッセージを受け取った人ごとに変更
+        // (今は環境変数に依存しているので固定の場所の天気しか取得できない)
+        $weatherForecaster = new WeatherForecaster(
+            $openWeatherMap,
+            env('OPENWEATHERMAP_LATITUDE'),
+            env('OPENWEATHERMAP_LONGITUDE')
+        );
+
+        return new ReplyMessageGenerator($weatherForecaster);
     }
 }
